19e38ff8c83f68b9b7097769a0cc24ff6a2ede9e
ajaydaram <ajayforchrist777@gmaol.com>
2023-07-13 10:31:20 +0800

    8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Enable CORS for all routes
 app.use(cors());
